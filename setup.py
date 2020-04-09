2c153a9b6d21877122e7d5dfcae798bd23f63797
Tran Viet Phuoc <phuoc.finn@gmail.com>
2020-04-09 18:49:26 +0700

    deploy app version 1.0

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="Facebook bot",
+    version='1.0',
+    packages=find_packages(),
+    author='Me',
+    author_email='phuoc.finn@gmail.com',
+    
+)
